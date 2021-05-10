c724ab899e5cec615fa06301d85b246be1cbeaab
Pablobalmis <pablo.martinezaznar@iesdoctorbalmis.com>
2021-05-10 10:28:42 +0200

    Funcionara??
    Spoiler no

--- leerFichero.js
+++ leerFichero.js
@@ -1,1 +1,14 @@
 function leerXMl()
+{
+if (window.XMLHttpRequest)
+  {
+  xhttp=new XMLHttpRequest();
+  }
+else
+  {
+  xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xhttp.open("GET",dname,false);
+xhttp.send();
+return xhttp.responseXML;
+}
