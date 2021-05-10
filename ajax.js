4ace418cd8f238f021456be86d086fa7ab030e72
Pablobalmis <pablo.martinezaznar@iesdoctorbalmis.com>
2021-05-10 10:25:27 +0200

    Vamos probando

---
+++ ajax.js
@@ -0,0 +1,20 @@
+function LoadLDoc(fichero, tipo)
+{
+    let http = new XMLHttpRequest();
+    http.open("GET", fichero, true);
+    http.setRequestHeader("Content-type", "text/xml");
+    http.send();
+    http.addEventListener('load', (event) => {
+        if(http.status == 200)
+        {
+            if(tipo == "xml")
+            {
+                leerXML(http.responseXML);
+            }
+            else
+            {
+                leerTXT(http.responseTXT);
+            }
+        }
+    })
+}
