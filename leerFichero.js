31666ba78de4e1b55057f66359b49c4b8a9195e3
Pablobalmis <pablo.martinezaznar@iesdoctorbalmis.com>
2021-05-12 08:43:21 +0200

    Prodria ser la primera vez que funcione??

--- leerFichero.js
+++ leerFichero.js
@@ -2,5 +2,9 @@ LoadLDocA("libros.xml", "xml");
 function leerXML(fichero)
 {
     let main = document.querySelector("main");
-    main.innerText = fichero;
+    let libros = fichero.querySelectorAll("titulo");
+    for(let i = 0; i < libros.length; i++)
+    {
+        main.innerHTML = main.innerHTML + "<p>" + libros[i].textContent + "</p>";
+    }
 }
