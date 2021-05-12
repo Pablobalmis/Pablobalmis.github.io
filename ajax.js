a5ad198549700e8538ed8c77b61ce34573a5409c
Pablobalmis <pablo.martinezaznar@iesdoctorbalmis.com>
2021-05-12 08:06:15 +0200

    Hacemos cosas

--- ajax.js
+++ ajax.js
@@ -1,4 +1,4 @@
-function LoadLDoc(fichero, tipo)
+function LoadLDocA(fichero, tipo)
 {
     let http = new XMLHttpRequest();
     http.open("GET", fichero, true);
@@ -19,17 +19,3 @@ function LoadLDoc(fichero, tipo)
     })
 }
 
-function leerXMl()
-{
-if (window.XMLHttpRequest)
-  {
-  xhttp=new XMLHttpRequest();
-  }
-else
-  {
-  xhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xhttp.open("GET",dname,false);
-xhttp.send();
-return xhttp.responseXML;
-}
