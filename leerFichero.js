661451013cacbafc70ec301224fa8da4c6d66d36
Pablobalmis <71265611+Pablobalmis@users.noreply.github.com>
2021-05-12 09:25:56 +0200

    Update leerFichero.js

--- leerFichero.js
+++ leerFichero.js
@@ -6,6 +6,6 @@ function leerXML(fichero)
     for(let i = 0; i < libros.length; i++)
     {
         console.log(libros[i].querySelector("ISBN").textContent);
-        tabla.innerHTML += "<div class='fila'> <div class='columna'>" + libros[i].querySelector("ISBN").textContent + "</div></div>";
+        tabla.innerHTML += "<div class='fila'> <div class='columna'>" + libros[i].querySelector("ISBN").textContent + "</div>" + "<div class='columna'>" + libros[i].querySelector("titulo").textContent + "</div>" + "<div class='columna'>" + libros[i].querySelector("nivelProfundidad").textContent + "</div>" + "<div class='columna'>" + libros[i].querySelector("autor").textContent + "</div>" + "<div class='columna'>" + libros[i].querySelector("editorial").textContent + "</div>" + "<div class='columna'>" + libros[i].querySelector("fechaPublicacion").textContent + "</div>" + "<div class='columna'>" + libros[i].querySelector("paginaWeb").textContent + "</div>" + "<div class='columna'>" + libros[i].querySelector("precio").textContent + "</div>" + "</div>";
     }
 }
