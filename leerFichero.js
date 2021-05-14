d46a3fd6d50200e9a862676726bf383d529507e4
Pablobalmis <pablo.martinezaznar@iesdoctorbalmis.com>
2021-05-14 08:09:47 +0200

    Update leerFichero.js

--- leerFichero.js
+++ leerFichero.js
@@ -22,7 +22,7 @@ function leerXML(fichero)
         console.log(libros[i].querySelector("ISBN").textContent);
         tabla.innerHTML += "<div class='fila'> <div class='columna'>" + libros[i].querySelector("ISBN").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("titulo").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("nivelProfundidad").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("autor").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("editorial").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("fechaPublicacion").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("paginaWeb").textContent + " " + "</div>";
         
-        if(menor == libros[i].querySelector("precio").textContent)
+        if(libroMasBarato == +libros[i].querySelector("precio").textContent)
         tabla.innerHTML += "<div class='columna barato'>" +  libros[i].querySelector("precio").textContent + "</div>" + "</div>";
         else
          tabla.innerHTML += "<div class='columna'>" +  libros[i].querySelector("precio").textContent + "</div>" + "</div>";
