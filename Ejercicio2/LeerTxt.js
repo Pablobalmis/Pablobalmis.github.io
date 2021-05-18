2a4af06c0bc9873f3ab3d69ae1531b9b83edd31d
Pablobalmis <pablo.martinezaznar@iesdoctorbalmis.com>
2021-05-18 08:47:58 +0200

    Lopis me ayudo, asi que va a funcionar

--- Ejercicio2/LeerTxt.js
+++ Ejercicio2/LeerTxt.js
@@ -3,15 +3,17 @@ function leerTXT(fichero)
 {
     alert(fichero);
     let lineas = fichero.split("/");
+    let izquierda = document.querySelector(".izquierda");
+    let derecha = document.querySelector(".derecha");
     for(let i = 0; i < lineas.length; i++)
     {
-        if(a%2 == 0)
+        if(i%2 == 0)
         {
-            
+            izquierda.innerHTML(lineas[i]);
         }
         else
         {
-            
+            derecha.innerHTML(lineas[i]);
         }
     }
 }
