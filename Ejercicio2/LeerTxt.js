a666a37f927fa7eb19513d6c889bcb23ee4e3ce7
Pablobalmis <pablo.martinezaznar@iesdoctorbalmis.com>
2021-05-18 09:00:11 +0200

    Empezamos a decorar :)

--- Ejercicio2/LeerTxt.js
+++ Ejercicio2/LeerTxt.js
@@ -1,7 +1,6 @@
 LoadLDocA("Mensajes.txt", "txt");
 function leerTXT(fichero)
 {
-    alert(fichero);
     let lineas = fichero.split("/");
     let izquierda = document.querySelector(".izquierda");
     let derecha = document.querySelector(".derecha");
