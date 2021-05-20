69b4a2c6c09527b1761769ccff4d192c21761c97
Pablobalmis <pablo.martinezaznar@iesdoctorbalmis.com>
2021-05-20 09:29:14 +0200

    Lo retocamos

--- Ejercicio1/leerFichero.js
+++ Ejercicio1/leerFichero.js
@@ -2,7 +2,6 @@ LoadLDocA("libros.xml", "xml");
 function PrecioMenor(libros)
 {
     let menor = Math.max;
-    let libroMasBarato;
     for(let i = 0; i < libros.length; i++)
     {
         if(libros[i].querySelector("precio").textContent < menor)
@@ -17,16 +16,14 @@ function leerXML(fichero)
     let tabla = document.querySelector(".tabla");
     let libros = fichero.querySelectorAll("libro");
     let libroMasBarato = PrecioMenor(libros);
-    console.log(libroMasBarato)
     for(let i = 0; i < libros.length; i++)
     {
-        tabla.innerHTML += "<div class='fila'> <div class='columna'>" + libros[i].querySelector("ISBN").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("titulo").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("nivelProfundidad").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("autor").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("editorial").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("fechaPublicacion").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("paginaWeb").textContent + " " + "</div>";
+        let fila = "<div class='fila'> <div class='columna'>" + libros[i].querySelector("ISBN").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("titulo").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("nivelProfundidad").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("autor").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("editorial").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("fechaPublicacion").textContent + " " + "</div>" + "<div class='columna'>" + libros[i].querySelector("paginaWeb").textContent + " " + "</div>";
         
         if(libroMasBarato == libros[i].querySelector("precio").textContent)
-        tabla.innerHTML += "<div class='columna-barato'>" +  libros[i].querySelector("precio").textContent + "</div>" + "</div>";
+            fila += "<div class='columna-barato'>" +  libros[i].querySelector("precio").textContent + "</div>" + "</div>";
         else
-         tabla.innerHTML += "<div class='columna'>" +  libros[i].querySelector("precio").textContent + "</div>" + "</div>";
-        
-        
+            fila += "<div class='columna'>" +  libros[i].querySelector("precio").textContent + "</div>" + "</div>";
+        tabla.innerHTML += fila;
     }
 }
